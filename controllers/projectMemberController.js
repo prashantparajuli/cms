ee907ca1ec78bc2fccd94890f2baf9f9895106a4
prashant parajuli <prashanta.parajuli@macpokhara.edu.np>
2022-07-17 10:10:36 +0545

    project member add/remove

--- controllers/projectMemberController.js
+++ controllers/projectMemberController.js
@@ -2,18 +2,30 @@ const { ProjectMember, Project, User } = require('../models');
 exports.addProjectMember = (req, res) => {
     res.render('./admin/project/addproject-member')
 }
-exports.getProjectMember = async(req,res) => {
-    ProjectMember.findAll({ include: [{ model: Project, as: 'project' },{ model: User, as: 'user' }] }).then((data) => {
-        console.log(data)
-        res.render('./admin/project/viewproject-member', { data: data });
-    }).catch((error) => {
-        res.send(error);
+exports.getProjectMember = async(req, res) => {
+    const data = await Project.findAll({
+        include: [{
+            model: ProjectMember,
+            as: 'projectMember',
+            include: [{
+                model: User,
+                as: 'user',
+                attributes: ['id', 'firstName', 'lastName']
+            }]
+        }]
     })
- }
+    res.render('./admin/project/viewproject-member', { data: data })
+        // ProjectMember.findAll({ include: [{ model: Project, as: 'project' },{ model: User, as: 'user' }] }).then((data) => {
+        //     console.log(data)
+        //     res.render('./admin/project/viewproject-member', { data: data });
+        // }).catch((error) => {
+        //     res.send(error);
+        // })
+}
 exports.postProjectMember = async(req, res) => {
     const data = req.body;
     ProjectMember.create(data).then((result) => {
-       // res.json({ message: 'added successfully', result });
+        // res.json({ message: 'added successfully', result });
         req.flash('info', 'project member added successfully');
         res.redirect('/project/project-member');
     }).catch((error) => {
@@ -23,7 +35,7 @@ exports.postProjectMember = async(req, res) => {
 
 }
 exports.deleteProjectMember = (req, res) => {
-    ProjectMember.destroy({ where: { id: req.params.id } }).then((data) => {
+    ProjectMember.destroy({ where: { userId: req.params.id } }).then((data) => {
         req.flash('info', 'deleted successfully');
         res.redirect('/project/project-member');
     })
--- models/projectmember.js
+++ models/projectmember.js
@@ -1,40 +1,40 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class ProjectMember extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-     static associate({ Project, User }) {
-      // define association here
-      this.belongsTo(Project, { foreignKey: 'id', as: 'project' })
-      this.belongsTo(User, { foreignKey: 'id', as: 'user' })
-  }
-  }
-  ProjectMember.init({
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-          model: 'User',
-          key: 'id'
-      }
-  },
-    projectId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-          model: 'Project',
-          key: 'id'
-      }
-  }
-  }, {
-    sequelize,
-    modelName: 'ProjectMember',
-  });
-  return ProjectMember;
+    class ProjectMember extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate({ Project, User }) {
+            // define association here
+            this.belongsTo(Project, { foreignKey: 'projectId', as: 'project' })
+            this.belongsTo(User, { foreignKey: 'userId', as: 'user' })
+        }
+    }
+    ProjectMember.init({
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'User',
+                key: 'id'
+            }
+        },
+        projectId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Project',
+                key: 'id'
+            }
+        }
+    }, {
+        sequelize,
+        modelName: 'ProjectMember',
+    });
+    return ProjectMember;
 };
